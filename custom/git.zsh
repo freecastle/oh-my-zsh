19999d4da2bd36e764450d01e8d3c6aa6b5a3342
Fabian Freiburg <f.freiburg@gmail.com>
2013-07-22 14:26:53 +0200

    simplified and disabled the faster parse_git_dirty()

--- custom/git.zsh
+++ custom/git.zsh
@@ -1,9 +1,4 @@
 # Checks if working tree is dirty
-parse_git_dirty() {
-#  if [[ -n $(git status -s 2> /dev/null) ]]; then
-#    echo "$ZSH_THEME_GIT_PROMPT_DIRTY"
-#  else
-    echo "$ZSH_THEME_GIT_PROMPT_CLEAN"
-#  fi
-}
- 
+#parse_git_dirty() {
+#  echo "$ZSH_THEME_GIT_PROMPT_CLEAN"
+#}
