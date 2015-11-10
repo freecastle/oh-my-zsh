c0f292502bdc1995c1c9fad5d2a1ac3d872c25e5
Fabian Freiburg <f.freiburg@gmail.com>
2015-11-10 17:10:14 +0100

    Updated iwhois according to iwantmyname/whois.geek.nz

--- plugins/iwhois/iwhois.plugin.zsh
+++ plugins/iwhois/iwhois.plugin.zsh
@@ -1,8 +1,13 @@
 # provide a whois command with a more accurate and up to date list of whois
 # servers using CNAMES via whois.geek.nz
 
+WHOIS=`which whois`
 function iwhois() {
-    resolver="whois.geek.nz"
+    resolver=".whois.geek.nz"
     tld=`echo ${@: -1} | awk -F "." '{print $NF}'`
-    whois -h ${tld}.${resolver} "$@" ;
+    if [ `echo ${tld} | egrep 'com|net|tv|cc'` ]; then
+        ${WHOIS} "$@"
+    else
+        ${WHOIS} -h ${tld}${resolver} "$@"
+    fi;
 }
